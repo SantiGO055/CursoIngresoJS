f898f89088a4fa747296b4bd87ba604f2cb1478f
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-03 12:45:45 -0300

    Ejercicio 8
    Ejercicio 8

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -1,8 +1,18 @@
-/*Debemos lograr tomar Los numeros por ID ,
+﻿/*Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+
+	var dividendo;
+	var divisor;
+	var resto;
+	dividendo=document.getElementById('numeroDividendo').value;
+	divisor=document.getElementById('numeroDivisor').value;
+	
+	resto=dividendo%divisor;
+	alert("El resto es: " +resto);
+	
 	
 }
