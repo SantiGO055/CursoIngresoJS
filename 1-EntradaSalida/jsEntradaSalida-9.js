79bf3b916310f077442d33b390a0688141eb3538
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-03 12:55:45 -0300

    Ejercicio 9
    Ejercicio 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -2,8 +2,20 @@
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
+
 function mostrarAumento()
 {
-
+	var sueldo;
+	var resultado;
+	var aumento;
 	
+	sueldo=document.getElementById('sueldo').value;
+	sueldo=parseInt(sueldo);
+	
+	resultado=(sueldo*0.10);
+	resultado=resultado+sueldo;
+	document.getElementById('resultado').value=resultado;
+	
+
+
 }
