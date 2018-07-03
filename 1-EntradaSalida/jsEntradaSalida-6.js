156bc80dbaebeb7ee06428799c9dcaa73723490c
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-03 11:36:44 -0300

    Ejercicio 6
    Ejercicio 6
    Todo bien hasta que me olvide de asignar el parseint a la variable numero entonces concatenaba los numeros, no los sumaba

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var numero1;
+	var numero2;
+	var resultado;
 
+	numero1=document.getElementById('numeroUno').value;
+	numero2=document.getElementById('numeroDos').value;
+	numero1=parseInt(numero1); /*Me habia olvidado de asignar el parseInt a numero1 entonces funcionaba pero me concatenaba los numeros, no los sumaba*/
+	numero2=parseInt(numero2);
+	
+        	resultado=(numero1+numero2);
+	alert("La suma de los numeros es: "+resultado);
 }
-
