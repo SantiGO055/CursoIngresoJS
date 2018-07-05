db61f395e6f6b6b3631ed56bf0b1b6a6261ff500
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-05 13:03:26 -0300

    ejercicio 4 sin terminar
    ejercicio 4 sin terminar

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+	var numero1, numero2, mayor;
+	numero1=prompt("Ingrese un numero");
+	numero2=prompt("Ingrese otro numero");
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+	if (numero1==numero2) {
+		numero1=numero1.toString();
+		numero2=numero1.toString();
+		alert(numero1+numero2);
+	}
+	if (numero1>numero2) {
+		mayor=numero1-numero2;
+		alert(mayor);
+		}
+		else {
+			suma=numero1+numero2;
+			alert(suma);
+			if (suma>10) {
+				
+			}
+		}
 
 }
