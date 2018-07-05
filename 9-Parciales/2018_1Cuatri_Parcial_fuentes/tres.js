94717c595b1acd6a3abbaa57e1f30c35fb95f2cc
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-05 12:37:14 -0300

    Ejercicios parcial
    Parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,11 @@
 function mostrar()
 {
-
+	var precio, descuento, preciofinal;
+	precio=prompt("Ingrese el precio");
+	descuento=prompt("Ingrese el porcentaje de descuento");
+	precio=parseInt(precio);
+	descuento=parseInt(descuento);
+	preciofinal=(precio*descuento)/100;
+	preciofinal=(precio+preciofinal);
+	document.getElementById('elPrecioFinal').value=preciofinal;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,9 @@
 
 function mostrar()
 {
-
+	var ancho, largo, perimetro;
+	ancho=prompt("Ingrese el ancho del rectangulo");
+	largo=prompt("Ingrese el largo del rectangulo");
+	perimetro=(ancho*2)+(largo*2)
+	alert("El perimetro del rectangulo es :"+perimetro);
 }
