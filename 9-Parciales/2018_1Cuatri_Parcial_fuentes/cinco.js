b50af5e692b0f7617f1d3e8ef87dbead491e897f
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-05 14:02:43 -0300

    Ejercicio 5
    Ejercicio 5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,34 @@
 function mostrar()
 {
+	var planeta;
+	planeta=prompt("Ingrese un planeta del sistema solar");
+	switch (planeta) {
+    case "mercurio":
+        alert("Acá hace más calor");
+        break;
+    case "venus":
+        alert("Acá hace más calor");
+        break;
+    case "tierra":
+        alert("Acá vivimos");
+        break;
+    case "marte":
+        alert("Acá hace mas frio");
+        break;
+    case "jupiter":
+        alert("Acá hace mas frio");
+        break;
+    case "saturno":
+        alert("Acá hace mas frio");
+        break;
+    case "urano":
+        alert("Acá hace mas frio");
+        break;
+    case "neptuno":
+        alert("Acá hace mas frio");
+        break;
+    default:
+    	alert("No es un planeta valido");
+	}
 
 }
