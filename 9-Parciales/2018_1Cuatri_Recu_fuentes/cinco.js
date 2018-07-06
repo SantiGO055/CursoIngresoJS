cce21d0fbc743d53e1247b96a807133f423a9ea6
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-06 14:44:54 -0300

    ejercicio 5 terminado
    ejercicio 5 terminado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
+	var dia;
+	dia=prompt("Ingrese un dia de la semana escrito en minuscula");
+	switch (dia){
+		case "lunes":
+		case "martes":
+		case "miercoles":
+		case "jueves":
+			alert("A trabajar");
+			break;
+		case "viernes":
+			alert("Buen finde");
+			break;
+		case "sabado":
+			alert("Buen finde");
+			break;
+		case "domingo":
+			alert("Buen finde");
+			break;
+		default:
+			alert("No ingresaste un dia");
+			break;
 
+	}
 }
