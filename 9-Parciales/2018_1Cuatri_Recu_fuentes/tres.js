945a5d850ee96c27f4f62b00f985f8ec60e6f826
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-05 21:55:17 -0300

    ejercicios incompletos

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
+	var precio
+	var descuento
+	var preciocondescuento
+	var iva;
+	precio=prompt("Ingrese el precio");
+	descuento=prompt("Ingrese el porcentaje de descuento");
+	precio=parseInt(precio);
+	descuento=parseInt(descuento);
 
+	descuento=(precio*descuento)/100;
+	preciocondescuento=(precio-descuento);
+	iva=(precio*21)/100;
+	iva=preciocondescuento+iva;
+	alert("El descuento es de "+descuento+" el precio con descuento es de "+preciocondescuento+" el precio con iva es de "+iva);
+	preciofinal=preciocondescuento+iva;
+	document.getElementById('elPrecioFinal').value=preciofinal;
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
-
+	var base, altura, perimetro, superficie;
+	base=prompt("Ingrese la base de un triangulo");
+	altura=prompt("Ingrese la altura de un triangulo");
+	
+	superficie=(base*altura)/2;
+	perimetro=(base*base)+(altura*altura);
+	perimetro=Math.sqrt(perimetro);
+	alert("La superficie del triangulo es :"+superficie+" y el perimetro del triangulo es"+perimetro);
+	
 }
