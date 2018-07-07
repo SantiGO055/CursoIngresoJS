51c445410978b0dbdae452ffa1f7b3e5ed43125e
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-07 09:09:26 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad, estadocivil;
+	edad=document.getElementById('edad').value;
+	estadocivil=document.getElementById('estadoCivil').value;
+	if (estadocivil != 'Soltero' && edad<18) {
+		alert("Es muy pequeño para NO ser soltero");
 
+	}
 	
 
 
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
+var edad, estadocivil;
+	edad=document.getElementById('edad').value;
+	estadocivil=document.getElementById('estadoCivil').value;
+	if (estadocivil != 'Soltero' && edad<18) {
+	}
+	else{
+		if (estadocivil == 'Soltero' && edad>17) {
+			alert("Es soltero y no es menor");
+		}
 
+	}
 	
 
 
