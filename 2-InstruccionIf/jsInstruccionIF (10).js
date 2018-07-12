33bd47bd2f202851d55bf2b3bbca0ce368f4d87a
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-12 20:12:31 -0300

    Termina3
    Termina3

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	numero=Math.round(Math.random()*10);
+	alert(numero);
+	if (numero<5) {
+		alert("Vamos, la proxima se puede");
+	}
+	else{
+		if (numero<9) {
+			alert("APROBÓ");
+		}
+		else{
+			alert("EXCELENTE");
+			}
+
+	}
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,8 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	numero=Math.round(Math.random()*10);
+	alert(numero);
 
 }//FIN DE LA FUNCIÓN
