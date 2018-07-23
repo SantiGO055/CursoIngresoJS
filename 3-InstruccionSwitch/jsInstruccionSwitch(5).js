c55be013039b6b74e196ac14895be18ec0ca88ca
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-23 20:25:31 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -2,9 +2,19 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
-
+laHora=parseInt(laHora);
 //alert (laHora);
-	
+	switch(laHora){
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+		alert("Es de mañana.");
+		break;
+	default:
+		alert("No es una hora valida");
+	}
 	
 
 
