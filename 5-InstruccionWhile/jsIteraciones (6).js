24df78de6bb8ed369cac681fc4a94d7eb4e90f00
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-26 20:18:25 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,8 +3,15 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	while(contador<5)
+	{
 
-
+		numero=prompt('Ingrese un numero');
+		numero=parseInt(numero);
+		acumulador=numero+acumulador;
+		contador++;
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,17 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+	numero=prompt('Ingrese numeros, para cortar escriba si');
+	while(numero>0 && numero!='si')
+	{
 
+		
+		numero=parseInt(numero);
+		acumulador=numero+acumulador;
+		numero=prompt('Ingrese numeros, para cortar escriba si');
+		contador++;
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
