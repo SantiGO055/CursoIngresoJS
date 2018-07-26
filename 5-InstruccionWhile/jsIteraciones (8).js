0e33f3863f4fc7ade9c030187e1d7156b2bad842
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-26 20:24:39 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,10 +4,22 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
+	var numero;
+	numero=prompt('Ingrese numeros, para cortar escriba si');
+	while(numero>0 ||numero<0 && (numero!='si'))
+	{
+		numero=parseInt(numero);
+		if (numero>0){
+			positivo=numero+positivo;
+		}
+		else{
+			negativo=numero*negativo;
+		}
 
 
+		numero=prompt('Ingrese numeros, para cortar escriba si');
+		contador++;
+	}
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
 
