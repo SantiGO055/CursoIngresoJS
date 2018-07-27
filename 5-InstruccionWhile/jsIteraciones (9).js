75204a97837d98a1d6f0b9ca9776f2521194a90f
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-26 22:11:09 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,16 +2,38 @@ function mostrar()
 {
 
 	var contador=0;
+	var numero;
+	var respuesta='si'
 	// declarar variables
 	
-	var respuesta='si';
-
+	
 	while(respuesta!='no')
 	{
-		
-	
-	}
+		numero=prompt('Ingrese un numero #'+contador);
+		numero=parseInt(numero);
 
+		if (contador==0) {
+			maximo=numero;
+			minimo=numero;
+		}
+		else{
+			if (numero>maximo) {
+				maximo=numero;
+			}
+			if(numero<minimo){
+				minimo=numero;
+			}
+		}
+
+
+
+
+
+		respuesta=prompt('Quiere continuar? Ingrese si o no');
+		contador++;
+	}
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
 
 
 
