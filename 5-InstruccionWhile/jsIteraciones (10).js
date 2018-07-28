da8a99f885af331e43118a02c57cc3097f953dac
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-28 10:50:20 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,16 +3,58 @@ function mostrar()
 
 	var contador=0;
 	//declarar contadores y variables 
-	
+	var negativos=0;
+	var positivos=0;
+	var pares;
+	var numero;
+	var cantpositivos=0;
+	var cantnegativos=0;
+	var cantceros=0;
+	var pares=0;
+	var impares=0;
+	var diferencia;
 	var respuesta="si";
 
 	while(respuesta!="no")
 	{
-		
-	
+		numero=prompt('Ingrese un numero');
+		numero=parseInt(numero);
+		//punto 1, 2, 3, 4 y 5
+		if (numero>0){
+			positivos=numero+positivos;
+			cantpositivos++;
+		}
+		else if (numero<0) {
+			negativos=numero+negativos;
+			cantnegativos++;
+		}
+		else {
+			cantceros++;
+		}
+		//punto 6
+		if (numero%2==0) {
+				pares++;
+		}
+		else{
+			impares++;
+		}
+
+
+		respuesta=prompt('¿Quiere continuar?');
+		contador++;
 	}
-
-
+	alert("La suma de los positivos es "+positivos);
+	alert("La suma de los negativos es "+negativos);
+	alert("La cantidad de positivos es de "+cantpositivos);
+	alert("La cantidad de negativos es de "+cantnegativos);
+	alert("La cantidad de ceros es de "+cantceros);
+	//punto 7
+	alert("El promedio de los positivos es "+positivos/cantpositivos);
+	//punto 8
+	alert("El promedio de los positivos es "+negativos/cantnegativos);
+	//punto 9
+	diferencia=positivos-negativos;
+	alert("La diferencia entre positivos y negativos es "+diferencia);
 
 
 }//FIN DE LA FUNCIÓN
