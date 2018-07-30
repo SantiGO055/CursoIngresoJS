df5b3731fd1c3ed4227dbd8f0fdf0b5d06cd09ce
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-07-30 20:04:27 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,12 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = prompt("ingrese el número de repeticiones");
+var i=0
+	for (i; i <repeticiones; i++) {
 
+		console.log("Hola UTN FRA");
+	}
 
 
 }//FIN DE LA FUNCIÓN
