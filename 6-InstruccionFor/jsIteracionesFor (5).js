9c9d30e8f5406ec2585ee6887835ce2b1b01cbde
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-08-02 20:17:11 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,7 +1,19 @@
 function mostrar()
 {
 
+	var contador;
+	var numero;
+	
+	for (contador=1; ;contador++) {
+		numero=prompt("Ingrese un numero");
+		console.log("Numero de pedido "+contador);
+		
+		if (numero>=9) {
+		break;
+		}
 
+	}
+	console.log("Los pedidos totales fueron "+contador);
 
 
 }//FIN DE LA FUNCIÓN
