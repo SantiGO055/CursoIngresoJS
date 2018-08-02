3ca625b45dab19861fdd0f880e879d4c2370859d
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-08-02 20:27:08 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 
+	var contador;
+	var numero;
+	var acumuladorPares=0;
+	numero=prompt("Ingrese un numero");
+	for (contador=1; contador<numero;contador++) {
+		
+		
+		if (contador%2==0) {
+
+		acumuladorPares++;
+		console.log("Los numeros pares que hay en "+numero+" son "+contador);
+		}
+		
+	}
+	console.log("La cantidad de numeros pares es "+acumuladorPares);
 
 
 
