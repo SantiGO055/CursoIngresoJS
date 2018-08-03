a8614946cce6d5ccf3eb272fb2534b4398c6ac42
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-08-02 22:05:23 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,6 +1,28 @@
 function mostrar()
 {
 
+	var numero;
+	var divisores=0;
+	var numeroAnterior;
+	var numerosRecorridos;
+	numero=prompt("Ingrese un numero");
+	numero=parseInt(numero);
+
+	for(numerosRecorridos=numero;numerosRecorridos>1;numerosRecorridos--)
+	{
+			divisores=0;
+			for (numeroAnterior=numerosRecorridos-1; numeroAnterior>1 ;numeroAnterior--) {		
+	//devuelve un 0 cuando es primo
+			if (numerosRecorridos%numeroAnterior==0) { 
+			divisores++; //encontre un divisor
+			break;
+			}	
+					
+		}
+		if (divisores==0) {
+			console.log("El numero es primo: "+numerosRecorridos);
+			}
+	}
 
 
 
